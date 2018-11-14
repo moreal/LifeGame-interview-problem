0320a05b15c4d3a8f17ce66489be10760bcb23ff
moreal <dev.moreal@gmail.com>
2018-11-14 20:04:26 +0900

    Add compiler option, std=c++17

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR"
 
-g++ -I ./Header `find ./Source/*.cc` -o run $@ >/dev/null
+g++ -std=c++17 -I ./Header `find ./Source/*.cc` -o run $@ >/dev/null
