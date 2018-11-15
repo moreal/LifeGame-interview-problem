41eba808dfdf429524c1c9220da31c389d406760
moreal <dev.moreal@gmail.com>
2018-11-15 14:28:59 +0900

    Add map_test some..

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -std=c++11 -I ./Header `find ./Source/*.cc` -o run $@
+g++ -I ./Header $(find ./Source/*.cc) -o run -std=c++11 $@
--- Test/map_test.cc
+++ Test/map_test.cc
@@ -4,7 +4,7 @@
 
 using loadcomplete::Map;
 
-TEST(map_test, FF) {
-
-    EXPECT_EQ(1, 1);
+TEST(map_test, change_generation_test) {
+    // TODO
+    // Data로 Input을 입력받는 것을 만들어야 할까요..?
 }
